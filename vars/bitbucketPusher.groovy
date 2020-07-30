4fc8f17ba22df759ba9e9d212f0f061f3c60b7d1
Digital Alchemist <den77033@gmail.com>
2020-07-30 11:20:57 +0300

    Update bitbucketPusher.groovy

--- vars/bitbucketPusher.groovy
+++ vars/bitbucketPusher.groovy
@@ -1,3 +1,19 @@
-def gitPush() {
+def gitPush(String credentials) {
+  
+      withCredentials([
+            usernamePassword(credentialsId: credentials,
+            usernameVariable: 'username',
+            passwordVariable: 'password')
+        ]) {
+            PASSWORD = password.collect { it }
+	    USER = username.collect { it }
+        }
+				  
+        PASSWORD = PASSWORD.toString().replace(',', '').replace('[', '').replace(']', '').replace(' ', '')
+        USER = USER.toString().replace(',', '').replace('[', '').replace(']', '').replace(' ', '')
+
+                
+        String username = USER
+        String password = PASSWORD
 
 }
