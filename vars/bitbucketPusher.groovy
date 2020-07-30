caa1ed3a30f120f50f5fe0ce1c71a6d6ae5123a6
Digital Alchemist <den77033@gmail.com>
2020-07-30 11:38:47 +0300

    Update bitbucketPusher.groovy

--- vars/bitbucketPusher.groovy
+++ vars/bitbucketPusher.groovy
@@ -1,4 +1,4 @@
-def gitPush(String credentials, String userEmail) {
+def gitPush(String credentials, String userEmail, String repository) {
   
       withCredentials([
             usernamePassword(credentialsId: credentials,
@@ -13,13 +13,15 @@ def gitPush(String credentials, String userEmail) {
         USER = USER.toString().replace(',', '').replace('[', '').replace(']', '').replace(' ', '')
 
                 
-        String username = USER
-        String password = PASSWORD
+        //String username = USER
+        //String password = PASSWORD
 	
 	sh "git config --global user.name $USER"
 	sh "git config --global user.email $userEmail"
 	
 	sh 'git add .'
 	sh "git commit -m 'new build $BUILD_NUMBER'  "
+	
+	sh "git push https://$USER:$PASSWORD@bitbucket.org/$USER/$repository HEAD:$BRANCH_NAME" 
 
 }
